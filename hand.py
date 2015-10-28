25de173ee918e6a105ddc903b32bf950c22c647e
Dana <djmedhaug@gmail.com>
2015-10-28 13:48:35 -0700

    update files

--- hand.py
+++ hand.py
@@ -2,10 +2,18 @@ class Hand(object):
     def __init__(self, name):
         self.name = name
         self.cards = []
- 
+
+    def __str__(self):
+        text = "\n%s's hand contains:\n" % self.name
+        for card in self.cards:
+            text += str(card) + " "
+        text += "\nHand value: " + str(self.get_value())
+
+        return text
+
     def add_card(self, card):
         self.cards.append(card)
- 
+
     def get_value(self):
         result = 0
         aces = 0
@@ -16,10 +24,11 @@ class Hand(object):
         if result + aces * 10 <= 21:
             result += aces * 10
         return result
- 
+
     def __str__(self):
-        text = "%s's hand contains:\n" % self.name
+        text = "\n%s's hand contains:\n" % self.name
         for card in self.cards:
             text += str(card) + " "
         text += "\nHand value: " + str(self.get_value())
+
         return text
