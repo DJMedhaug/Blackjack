488bee32e1ae8242e7f4334b416b79297af7813f
Dana <djmedhaug@gmail.com>
2015-11-02 07:25:50 -0800

    Final commit

--- deck.py
+++ deck.py
@@ -3,6 +3,8 @@ from random import shuffle
 
 
 class Deck(object):
+    """ deck class that shuffles and distributes cards.
+    """
     def __init__(self):
         suits = (" Hearts", " Spades", " Diamonds", " Clubs")
         ranks = ("A", "2", "3", "4", "5", "6", "7", "8", "9", "T",
@@ -11,9 +13,3 @@ class Deck(object):
         shuffle(self.cards)
 
     def deal_card(self): return self.cards.pop()
-
-    
-#         self.test_hand1 = {0: 'Ace of Spades', 1: 'Jack of Clubs' } # for testing
-#         self.test_hand2 = {0: 'Ace of Clubs', 1: '10 of Diamonds' } # for testing
-#         self.test_hand3 = {0: '8 of Spades', 1: '8 of Hearts'} # for testing splits
-# ​
